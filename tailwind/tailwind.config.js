003d257a623274a86e22b86e003e8abc918a5a3f
Aroa-web <aroa.rivas.2003@gmail.com>
2024-11-26 09:01:22 +0100

    nuevo

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,22 +2,30 @@
 module.exports = {
   content: ["./webs/**/*.{html,js}"], /* webs es una carpeta en el nivel de tailwind.config.js */
   theme: {
-  extend: {
-    colors:{
-      'azul-claro':'#00ffff',
-      'azul-oscuro':'#0011ff'
+    extend: {
+      colors: {
+        'azul-claro': '#00ffff',
+        'azul-oscuro': '#0011ff'
+      },
+      spacing: {
+        '41': '800px;'
+      },
+      width: {
+        '39': '430px'
+      },
+      height: {
+        '39': '333px'
+      },
+      screens: {
+        'tablet': '900px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
     },
-    spacing:{
-      '41':'800px;'
-    },
-    width:{
-      '39':'430px'
-    },
-    height:{
-      '39':'333px'
-    }
-  },
   },
   plugins: [],
- }
- 
+}
+
