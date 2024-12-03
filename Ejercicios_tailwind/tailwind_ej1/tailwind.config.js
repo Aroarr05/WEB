9c9b303916508d6a0f8cf296e58bc904646d8e7e
Aroa-web <aroa.rivas.2003@gmail.com>
2024-12-03 10:47:29 +0100

    tailwind ejercicio1 terminado

--- Ejercicios_tailwind/tailwind_ej1/tailwind.config.js
+++ Ejercicios_tailwind/tailwind_ej1/tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["./webs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'azul-claro': '#00ffff',
+        'azul-oscuro': '#0011ff',
+      },
+      spacing: {
+        '41': '800px',
+      },
+      width: {
+        '39': '430px',
+      },
+      height: {
+        '39': '333px',
+      },
+      screens: {
+        'tablet': '900px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
   },
-  plugins: [],
-}
+};
+
 
